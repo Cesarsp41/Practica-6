08bbe7aa2da8b956383b228d7a57fdcb28c99481
Cesarsp41 <64189944+Cesarsp41@users.noreply.github.com>
2022-05-16 16:56:48 -0500

    Update Practica6.py

--- Practica6.py
+++ Practica6.py
@@ -2,4 +2,7 @@ import numpy as np
 import cv2
 
 cam = cv2.VideoCapture(1)
-print('Presiona "espacio" para cerrar el video.')
+
+#Colores (tonos)
+amarilloBajo = np.array([22,109,20],np.uint8)
+amarilloAlto = np.array([32,255,255],np.uint8)
